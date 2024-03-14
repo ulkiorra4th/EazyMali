ce47f07d9daa90bb2bd828f0ccbaf85a75671aff
ulkiorra4th <93437745+ulkiorra4th@users.noreply.github.com>
2024-03-14 14:39:39 +0300

    Delete ConsoleApp1 directory

--- ConsoleApp1/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Net.Mail;
-using MailChecker.Content;
-using MailChecker.Net;
-
-IMailContentBuilder contentBuilder = new MailContentBuilder();
-var content = contentBuilder.BuildMailContentFromHtml("somefilepath", "https://somelink");
-
-IMailSender sender = new MailSender(new MailAddress("ramin.muhtarov@gmail.com", "ЛОХ"), "icue xxkz rbib ntso", "smtp.gmail.com", 587);
-sender.SendMailAsync(new MailAddress("someAddress"),content);
