054ed25fbe7ff36d718c4147d13bb4fd0f675ad3
ulkiorra <ulkiorra_bsns@gmail.com>
2024-03-14 14:35:56 +0300

    1.0.3

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Net.Mail;
+using MailChecker.Content;
+using MailChecker.Net;
+
+IMailContentBuilder contentBuilder = new MailContentBuilder();
+var content = contentBuilder.BuildMailContentFromHtml("somefilepath", "https://somelink");
+
+IMailSender sender = new MailSender(new MailAddress("ramin.muhtarov@gmail.com", "ЛОХ"), "icue xxkz rbib ntso", "smtp.gmail.com", 587);
+sender.SendMailAsync(new MailAddress("someAddress"),content);
